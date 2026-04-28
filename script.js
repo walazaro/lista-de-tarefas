9e1bdabd19888a157b8cec1f81c368d71ef0b19b
walazaro <100889062+walazaro@users.noreply.github.com>
2026-04-28 09:49:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function adicionarTarefa() {
+    let input = document.getElementById("tarefa");
+    let texto = input.value;
 
+    if (texto === "") {
+        alert("Digite uma tarefa!");
+        return;
+    }
+
+    let lista = document.getElementById("lista");
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        ${texto}
+        <span class="remover" onclick="this.parentElement.remove()">X</span>
+    `;
+
+    lista.appendChild(li);
+    input.value = "";
+}
